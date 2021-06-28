c65fc5960f3f03fbcc6d178aeefd758057366a82
Nabil Ragab <nabil@ragab.net>
2021-06-29 00:10:39 +0200

    published_can be improved to add to web ui fonts selection

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gotham Pro Font',
+    'summary': 'Enable Gotham Pro Font',
+    'sequence': '7080',
+    'category': 'Hidden',
+    'description': """This module adds Gotham Pro WebFont to Odoo""",
+    'depends': ['web', 'website', 'portal'],
+    'data': [
+        'views/frontend_assets.xml',
+   ],
+}
