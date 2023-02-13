9c60d00b64fdfe984e03fb62b1a86adcf28ecae6
Nabil Ragab <nabil@ragab.net>
2023-02-13 22:32:32 +0200

    [MIG] v14
    -bump
    -update license
    -manifest to new company

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,40 @@
 # -*- coding: utf-8 -*-
+#################################################################################
+# Author      : ROOK FZ LLC (<https://rook.ae/>)
+# Copyright(c): 2020-Present ROOK.ae
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://rook.ae/license/>
+#################################################################################
 {
     'name': 'Gotham Pro Font',
     'summary': 'Enable Gotham Pro Font',
-    'sequence': '7080',
+    'description': 'This module adds Gotham Pro WebFont to Odoo',
     'category': 'Hidden',
-    'description': """This module adds Gotham Pro WebFont to Odoo""",
-    'depends': ['web', 'website', 'portal'],
+    'version': '14.0.1.0.0',
+    'author': 'Nabil Mohamed Ali Ragab',
+    'company': 'ROOK FZ LLC',
+    'maintainer': 'ROOK FZ LLC',
+    'website': "https://www.rook.ae",
+    'depends': [
+        'web',
+        'website',
+        'portal'
+    ],
     'data': [
         'views/frontend_assets.xml',
-   ],
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'Other proprietary',
+    'images': ['static/description/banner.jpg'],
+    'currency': 'USD',
+    'price': 0,
+    'support': 'nabil@rook.ae',
+
 }
